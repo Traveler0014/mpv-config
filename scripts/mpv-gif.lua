ddd9930c58d1471dc78c4de4cb0e35d0ab975189
Travon <zhouyanbai@qq.com>
2024-11-04 21:21:27 +0800

    config: adjust gif path for two

--- scripts/mpv-gif.lua
+++ scripts/mpv-gif.lua
@@ -10,7 +10,7 @@ require 'mp.options'
 local msg = require 'mp.msg'
 
 local options = {
-    dir = "C:/Program Files/mpv/gifs",
+    dir = "C:/Users/zhouy/Pictures/screenshots/gifs",
     rez = 600,
     fps = 15,
 }
